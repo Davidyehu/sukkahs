0b4e70e61473cae0ac0adfcc8b7c7cbf38aebbf6
Davidyehu <dyb613@gmail.com>
2023-09-19 03:37:30 -0400

    analytics + 404 for non generated pages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,6 +4,8 @@ import MainCanvas from "@/components/three-components/canvas";
 import styles from "./page.module.scss";
 import Sukkah from "@/components/three-components/sukkah";
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   return scenarios.map((scenario) => ({
     params: { slug: scenario.slug },
@@ -14,11 +16,23 @@ export default function Halacha({ params }: { params: { slug: string } }) {
   const scenario = scenarios.find((scenario) => params.slug == scenario.slug);
   return (
     <div className={styles.container}>
-      <h1>{scenarios.find((scenario) => params.slug == scenario.slug)?.description}</h1>
+      <h1>
+        {
+          scenarios.find((scenario) => params.slug == scenario.slug)
+            ?.description
+        }
+      </h1>
       <div className={styles.canvasContainer}>
         {scenario ? (
-          <MainCanvas pointLightPosition={scenario.props.pointLightPosition} pointLightIntensity={scenario.props.pointLightIntensity}>
-            <Sukkah dimensions={scenario?.props.dimensions} walls={scenario.props.walls} schach={scenario.props.schach}/>
+          <MainCanvas
+            pointLightPosition={scenario.props.pointLightPosition}
+            pointLightIntensity={scenario.props.pointLightIntensity}
+          >
+            <Sukkah
+              dimensions={scenario?.props.dimensions}
+              walls={scenario.props.walls}
+              schach={scenario.props.schach}
+            />
           </MainCanvas>
         ) : (
           "Loading..."
