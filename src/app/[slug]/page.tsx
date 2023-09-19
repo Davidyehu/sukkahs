7b55e3f44b2af1d3c82bc30078a04430ff055ee3
Davidyehu <dyb613@gmail.com>
2023-09-19 03:09:29 -0400

    static generation

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,6 +4,12 @@ import MainCanvas from "@/components/three-components/canvas";
 import styles from "./page.module.scss";
 import Sukkah from "@/components/three-components/sukkah";
 
+export async function generateStaticParams() {
+  return scenarios.map((scenario) => ({
+    params: { slug: scenario.slug },
+  }));
+}
+
 export default function Halacha({ params }: { params: { slug: string } }) {
   const scenario = scenarios.find((scenario) => params.slug == scenario.slug);
   return (
